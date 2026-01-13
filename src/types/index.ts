a283bcc46419885757165684b4de7a9901d8d546
Babla Dey <babladey275@gmail.com>
2026-01-13 17:48:27 +0600

    added AddTour form with full create tour functionality

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,3 +19,32 @@ export interface ISidebarItem {
 }
 
 export type TRole = "SUPER_ADMIN" | "ADMIN" | "USER";
+
+type ZodIssue = {
+  code: string;
+  expected: string;
+  received: string;
+  path: string[];
+  message: string;
+};
+
+type ErrorSource = {
+  path: string;
+  message: string;
+};
+
+interface IErrorResponse {
+  success: boolean;
+  message: string;
+  errorSources?: ErrorSource[];
+  err?: {
+    issues: ZodIssue[];
+    name: string;
+  };
+  stack?: string;
+}
+
+export interface IApiError {
+  status?: number;
+  data?: IErrorResponse;
+}
